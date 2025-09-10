38813c47f8ac4a0967d5e14f9148a02e0fd16e20
zanele maseko <itszanele@gmail.com>
2025-09-10 21:49:26 +0200

    changes

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,6 +3,9 @@ let number = parseFloat(process.argv[2]);
 if (number >0 ) {
     console.log(`My number: ${Math.floor(number)}`);
 }
+else if (number < 0) {
+    console.log(`My number: ${Math.ceil(number)}`);
+}
 
 else {
     console.log('Not a number')
