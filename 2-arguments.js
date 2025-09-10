da92f34a8b47fc1ddffea7af4ddf8bf1d3f9f850
zanele maseko <itszanele@gmail.com>
2025-09-10 18:09:52 +0200

    test

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const num= [1,2,3,4,5];
+
+if (num.length ==0) {
+    console.log("No argument");
+}
+else if (num==1) {
+    console.log("Argument found");
+}
+else {
+    console.log("Argument found");
+}
