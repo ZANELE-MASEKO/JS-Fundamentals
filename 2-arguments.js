c7be7a74485fae7413c251a547dc86b225bc6a55
zanele maseko <itszanele@gmail.com>
2025-09-10 18:34:34 +0200

    changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,4 @@
-const num= [];
+/*const num= [];
 
 if (num.length ==0) {
     console.log("No argument");
@@ -8,4 +8,16 @@ else if (num.length==1) {
 }
 else {
     console.log("Arguments found");
+}*/
+const args = process.argv.slice(2);
+const numArgvs= args.length;
+
+if (numArgvs===0) {
+    console.log('No argument');
+}
+else if (numArgvs ===1) {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found')
 }
