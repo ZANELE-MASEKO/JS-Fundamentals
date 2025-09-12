42fac7792f63398ecd38eaa96300b9256f82279f
zanele maseko <itszanele@gmail.com>
2025-09-12 19:54:19 +0200

    file seven

---
+++ 7-multi_c.js
@@ -0,0 +1,14 @@
+let num1= process.argv[2];
+if (!num1) {
+    console.log('missing number of occurences');
+}
+else {
+    num1= Number(num1);
+     for (let x= 0; x<num1; x++) {
+    console.log('C is fun');
+}
+}
+
+  
+    
+ 
