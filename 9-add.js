d38eecc6402627809fe68387bfe68021f5a64358
zanele maseko <itszanele@gmail.com>
2025-09-12 19:34:21 +0200

    file nine

--- 9-add.js
+++ 9-add.js
@@ -1,6 +1,9 @@
+
+
+function addition(one, two) {
+    return one +two ;
+}
+
 let num1= process.argv[2];
 let num2= process.argv[3];
-
-function addition(num1, num2) {
-    console.log(num1 + num2);
-}
+console.log(addition(parseFloat(num1), parseFloat(num2)));
